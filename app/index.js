3201d19f65dd9a47f2e42d48581759dead988b48
Juan De Jesus <jdejesus93@gmail.com>
2021-11-27 22:53:25 -0600

    Instance p2p server on index

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,3 +22,4 @@ app.post('/mine', (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
