4418fa5410a70fcfee8c6e6a9204badda78a7482
Juan De Jesus <jdejesus93@gmail.com>
2021-11-30 00:13:27 -0600

    Demo of wallet feature

--- dev-test.js
+++ dev-test.js
@@ -1,7 +1,3 @@
-const Blockchain = require('./blockchain');
-
-const bc = new Blockchain();
-
-for (let i=1; i < 10; i++) {
-    console.log(bc.addBlock(`foo ${i}`).toString());
-}
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
