23f182e0d3339e140f19745e0b87ea4709fb1884
Juan De Jesus <jdejesus93@gmail.com>
2021-11-30 02:07:03 -0600

    Create mine-transactions endpoint

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -11,6 +12,7 @@ const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -23,6 +25,12 @@ app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()}`);
     p2pServer.syncChains();
+    res.redirect('/blocks');
+});
+
+app.get('/mine-transactions', (req, res) => {
+    const block = miner.mine();
+    console.log(`New block added: ${block.toString()}`);
     res.redirect('/blocks');
 });
 
